65f04fd2d8dabec9624993ea7a72422e1346544e
PooperMario <126058096+PooperMario@users.noreply.github.com>
2023-02-27 04:00:39 -0500

    Initial commit

---
+++ App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const GameEmbed = () => {
+  return (
+    <div>
+      <h1>Bera Biking!!</h1>
+      <iframe frameborder="0" src="https://itch.io/embed-upload/7429218?color=333333" allowfullscreen="" width="720" height="500"><a href="https://pooncats.itch.io/bera-biking">Play Bera Biking on itch.io</a></iframe>
+    </div>
+  );
+};
+
+export default GameEmbed;
---
+++ README.md
@@ -0,0 +1,3 @@
+# BeraBiking
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/react-ts-k1edoq)
