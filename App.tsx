0b69cd041537863e6a325844644bf69f95623df2
PooperMario <126058096+PooperMario@users.noreply.github.com>
2023-02-28 20:24:27 -0500

    2 files modified

--- App.tsx
+++ App.tsx
@@ -3,7 +3,7 @@ import './style.css';
 
 const GameEmbed = () => {
   return (
-    <div>
+    <div className="container">
       <h1 style={{color: '', fontFamily: 'Courier New'}}>Bera Biking!!</h1>
       <iframe frameborder="0" src="https://itch.io/embed-upload/7429218?color=333333" allowfullscreen="" width="1600" height="900"><a href="https://pooncats.itch.io/bera-biking">Play Bera Biking on itch.io</a></iframe>
     </div>
